1e372a8fd9266a70904259caf1e60f0e8d29c857
Alen Alex <alen.alex@outlook.com>
2017-07-25 01:36:42 +0530

    Adding Tuples example with test cases

---
+++ TuplesExample/TuplesLib/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TuplesLib
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string EmailId { get; set; }
+
+        public (string firstName, string lastName) GetName() => (FirstName, LastName);
+
+        public void Deconstruct(out string firstName, out string lastName)
+        {
+            firstName = FirstName;
+            lastName = LastName;
+        }
+
+        public void Deconstruct(out string firstName, out string lastName, out int age)
+        {
+            Deconstruct(out firstName, out lastName);
+            age = Age;
+        }
+    }
+}
